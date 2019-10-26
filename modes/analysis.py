bd708d3e97dfff332187dcb780845d4db88b33a1
Jlobblet <jlobblet-github@gmx.co.uk>
2019-10-26 07:17:33 +0100

    Update analysis and functions

--- modes/analysis.py
+++ modes/analysis.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import igraph as ig
+import louvain as lv
 
 #from config.CONFIG import CONFIG
 from .functions import create_set
@@ -9,10 +11,21 @@ data_loc = r"data/aggregate/week1.csv"
 def read_raw_data(data_loc=data_loc):
     df = pd.read_csv(data_loc)
     df = df.dropna().reset_index().loc[:, ["Deck 1 List", "Deck 2 List", "Deck 3 List"]]
-
-    print(df.applymap(create_set))
+    df = df.replace("’", "'")
+    df = df.applymap(create_set)
 
     # print(df)
+    return df
+
 
 def main():
-    read_raw_data()
+    df = read_raw_data()
+    all_cards = set()
+    for index, row in df.iterrows():
+        for deck in row:
+            all_cards = all_cards | deck
+    card_data_df = pd.DataFrame(all_cards, columns=["Card"]).sort_values(by="Card")
+    card_data_df["Count"] = 0
+    # card_data_df["Decks"]
+    # card_data_df["Decks"] = card_data_df["Decks"].astype(object)
+    print(card_data_df)
--- modes/functions.py
+++ modes/functions.py
@@ -2,14 +2,21 @@ from os.path import join
 
 import regex as re
 
-decklist_line = r"^\d+x?[^\S\n\r]+([\w \-,/]*[^\s(\n])"
+decklist_line_1 = r"^([\w '\-,/]*[^\s(\n]) x\d+"
+decklist_line_2 = r"^\d+x?[^\S\n\r]+([\w '\-,/]*[^\s(\n])"
 basics = {"Plains", "Island", "Swamp", "Mountain", "Forest"}
 
 
 def create_set(text):
+
+    text = re.sub("['`]", "'", text)
+
     names = (
-        set(re.findall(decklist_line, text, flags=re.MULTILINE)) - basics
+        set(re.findall(decklist_line_1, text, flags=re.MULTILINE)) | set(re.findall(decklist_line_2, text, flags=re.MULTILINE)) - basics
     )
+
+    names = {name.title() for name in names}
+
     return names
 
 
