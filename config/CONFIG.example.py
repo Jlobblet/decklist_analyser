e7193c2e021017dc734df7ff4a3eaf2c426769f7
Jlobblet <jlobblet@gmx.com>
2019-10-29 18:30:33 +0000

    Update example config

--- config/CONFIG.example.py
+++ config/CONFIG.example.py
@@ -1,7 +1,7 @@
 CONFIG = {
-    "log_location": r"\logs\{}.log",
-    "decklist_directory": r"\data\decklists",
-    "aggregate_data_loc": r"data/aggregate/data.csv",
+    "log_location": r"logs/{}.log",
+    "decklist_directory": r"/data/decklists",
+    "aggregate_data_loc": r"data/aggregate/week1.csv",
     "basics": {"Plains", "Island", "Swamp", "Mountain", "Forest"},
     "cluster_percentage": 0.4,
     "deck_percentage": 0.5,
